a657124619135cd321e9750e316704f256b83888
OverripeWings3 <sjacobust@gmail.com>
2018-04-04 22:53:48 -0500

    Home tables/Spanish Strings

--- app/src/main/java/com/iteso/is699367/halp_exclamation_mark/ActivityLogin.java
+++ app/src/main/java/com/iteso/is699367/halp_exclamation_mark/ActivityLogin.java
@@ -3,11 +3,11 @@ package com.iteso.is699367.halp_exclamation_mark;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class ActivitySignIn extends AppCompatActivity {
+public class ActivityLogin extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_sign_in);
+        setContentView(R.layout.activity_login);
     }
 }
