fb1e411d30219fb748897b8bb95bde57eedebf6c
OverripeWings3 <sjacobust@gmail.com>
2018-04-05 02:58:04 -0500

    Splash/Login/Home

--- app/src/main/java/com/iteso/is699367/halp_exclamation_mark/beans/ActivityClases.java
+++ app/src/main/java/com/iteso/is699367/halp_exclamation_mark/beans/ActivityClases.java
@@ -1,6 +1,5 @@
-package com.iteso.is699367.halp_exclamation_mark;
+package com.iteso.is699367.halp_exclamation_mark.beans;
 
-import android.content.Intent;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
@@ -10,42 +9,34 @@ import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
 
-import com.iteso.is699367.halp_exclamation_mark.beans.ActivityClases;
+import com.iteso.is699367.halp_exclamation_mark.R;
 
-public class ActivityMain extends AppCompatActivity {
+public class ActivityClases extends AppCompatActivity {
+
 
     Toolbar toolbar;
     private DrawerLayout mDrawerLayout;
-    Intent intent;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_clases);
 
-        toolbar = findViewById(R.id.activity_main_toolbar);
-        mDrawerLayout = findViewById(R.id.activity_main_drawer_layout);
+        toolbar = findViewById(R.id.activity_clases_toolbar);
+        mDrawerLayout = findViewById(R.id.activity_clases_drawer_layout);
 
-        NavigationView navigationView = findViewById(R.id.nav_view);
+        NavigationView navigationView = findViewById(R.id.activity_clases_nav_view);
         navigationView.setNavigationItemSelectedListener(
                 new NavigationView.OnNavigationItemSelectedListener() {
                     @Override
-
                     public boolean onNavigationItemSelected(MenuItem menuItem) {
                         // set item as selected to persist highlight
-
                         menuItem.setChecked(true);
                         // close drawer when item is tapped
                         mDrawerLayout.closeDrawers();
+
                         // Add code here to update the UI based on the item selected
                         // For example, swap UI fragments here
-                        switch(menuItem.getItemId()) {
-                            case R.id.menu_class_schedule:
-                                intent = new Intent(ActivityMain.this, ActivityClases.class);
-                                startActivity(intent);
-                                finish();
-                                break;
-                        }
                         return true;
                     }
                 });
@@ -65,5 +56,4 @@ public class ActivityMain extends AppCompatActivity {
 
         return super.onOptionsItemSelected(item);
     }
-
 }
