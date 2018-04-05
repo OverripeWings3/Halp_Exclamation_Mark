2c00ac32e688ec5bd22bea4236d5bc126a132560
OverripeWings3 <sjacobust@gmail.com>
2018-04-05 00:36:49 -0500

    Tablas/Primera vista

--- app/src/main/java/com/iteso/is699367/halp_exclamation_mark/ActivityMain.java
+++ app/src/main/java/com/iteso/is699367/halp_exclamation_mark/ActivityMain.java
@@ -1,13 +1,24 @@
 package com.iteso.is699367.halp_exclamation_mark;
 
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 
 public class ActivityMain extends AppCompatActivity {
+
+    Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        toolbar = findViewById(R.id.activity_main_toolbar);
+        setSupportActionBar(toolbar);
+        ActionBar actionbar = getSupportActionBar();
+        actionbar.setDisplayHomeAsUpEnabled(true);
+        actionbar.setHomeAsUpIndicator(R.drawable.ic_menu_black_24dp);
+
     }
 }
